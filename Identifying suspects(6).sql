373e34a1fd536ef92dee9e096a315e175b1893e1
GonaSaicharan <156038924+GonaSaicharan@users.noreply.github.com>
2025-11-29 18:13:25 +0530

    Update and rename Identifying suspects.sql to Identifying suspects(6).sql

--- Identifying suspects(6).sql
+++ Identifying suspects(6).sql
@@ -13,8 +13,9 @@ WHERE call_time BETWEEN '2025-10-15 20:45' AND '2025-10-15 21:05'
 )
 SELECT DISTINCT emp.employee_id, emp.name
 FROM employees emp
-JOIN ceo_access ca ON emp.employee_id = ca.employee_id
-LEFT JOIN alibis a ON emp.employee_id = a.employee_id
-LEFT JOIN calls c ON emp.employee_id IN (c.caller_id, c.receiver_id)
-WHERE emp.employee_id IN (SELECT employee_id FROM ceo_access)
+JOIN ceo_access ca ON emp.employee_id=ca.employee_id
+LEFT JOIN alibis a ON emp.employee_id=a.employee_id
+LEFT JOIN calls c ON emp.employee_id IN(c.caller_id, c.receiver_id)
+WHERE emp.employee_id IN(SELECT employee_id FROM ceo_access)
 ORDER BY emp.employee_id;
+
