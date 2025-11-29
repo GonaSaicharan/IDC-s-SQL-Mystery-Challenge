a5e5691b284d08f2cbee74f3ed2dc51c99a8f260
GonaSaicharan <156038924+GonaSaicharan@users.noreply.github.com>
2025-11-29 18:10:47 +0530

    Update and rename Suspicious calls.sql to Suspicious calls(4).sql

--- Suspicious calls(4).sql
+++ Suspicious calls(4).sql
@@ -1,5 +1,6 @@
-SELECT c.call_id, c.caller_id, caller.name AS caller_name, c.receiver_id, receiver.name AS receiver_name, c.call_time, c.duration_secFROM alls c
+SELECT c.call_id, c.caller_id, caller.name AS caller_name, c.receiver_id, receiver.name AS receiver_name, c.call_time, c.duration_sec FROM alls c
 JOIN employees caller ON c.caller_id=caller.employee_id
 JOIN employees receiver ON c.receiver_id=receiver.employee_id
 WHERE c.call_time BETWEEN '2025-10-15 20:50' AND '2025-10-15 21:00'
 ORDER BY c.call_time;
+
